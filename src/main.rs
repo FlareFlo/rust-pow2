1e69c7d05853a67bdbb4ac7338eef780523020f2
FlareFlo <mail@flareflo.dev>
2024-05-24 02:07:14 +0200

    Take all plants for lesser permutations

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
 
     let mut new = vec![];
     new.extend(breed::<4, _>(plants.clone().into_iter().take(15)));
-    new.extend(breed::<3, _>(plants.clone().into_iter().take(15)));
-    new.extend(breed::<2, _>(plants.clone().into_iter().take(15)));
+    new.extend(breed::<3, _>(plants.clone().into_iter()));
+    new.extend(breed::<2, _>(plants.clone().into_iter()));
     new.sort_unstable_by_key(|e| e.score());
 
     println!("Top 10 plants:");
