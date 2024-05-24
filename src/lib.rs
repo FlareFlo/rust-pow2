c635a479391304d89a8e4c8ee881934d88533b0f
FlareFlo <mail@flareflo.dev>
2024-05-24 22:57:35 +0200

    Wai is actually not maintained so this was useless

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::fmt::Debug;
 use std::time::Instant;
 use itertools::Itertools;
 use crate::crossbreeder::Crossbreeder;
-use crate::plant::Plant;
 use crate::traits::PlantImpl;
 
 pub mod crossbreeder;
@@ -10,25 +9,6 @@ mod gene;
 pub mod plant;
 mod tests;
 pub mod traits;
-
-wai_bindgen_rust::export!("rust_pow2.wai");
-
-pub struct RustPow2 {}
-
-impl rust_pow2::RustPow2 for RustPow2 {
-	fn crossbreed(input: String) -> Vec<String> {
-		let mut plants = Plant::from_strings(&input);
-		plants.sort_unstable_by_key(|e| e.score());
-		plants.reverse();
-
-		let mut new = vec![];
-		new.extend(breed::<4, _>(plants.clone().into_iter().take(15)));
-		new.extend(breed::<3, _>(plants.clone().into_iter()));
-		new.extend(breed::<2, _>(plants.clone().into_iter()));
-		new.sort_unstable_by_key(|e| e.score());
-		new.into_iter().rev().dedup().take(10).map(|e|e.to_string()).collect()
-	}
-}
 
 pub fn breed<const PERMUTATIONS: usize, T: PlantImpl + Clone + Sized + Debug>(
 	plants: impl Iterator<Item = T>,
